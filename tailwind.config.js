56b15e6be616d6fe5fe7bf85fb54e8e852185664
adem <adem.selmani@esprit.tn>
2025-03-06 23:28:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4169E1",
+        "primary-dark": "#3153B6",
+        "page-bg": "#F7F3EE",
+        "custom-bg": "#F7F3EE",
+        "custom-button": "#4169E1",
+        "custom-endcall": "#EB5757",
+        "custom-text": "#000000",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
